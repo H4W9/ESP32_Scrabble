a8f52a8ef4d35f7e98e00370e787236acac5d078
H4W9 <hoferwade@gmail.com>
2026-08-09 19:02:17 -0500

    Update configs.h

--- configs.h
+++ configs.h
@@ -17,7 +17,7 @@
 // time; it stays "dev" for local builds.
 #define FW_NAME    "Scrabble"
 #define FW_AUTHOR  "H4W9"
-#define FW_VERSION "0.1.0"
+#define FW_VERSION "0.1.1"
 #ifndef FW_COMMIT
 #define FW_COMMIT  "dev"
 #endif
