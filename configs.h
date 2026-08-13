e75f540b987ca99383090af14d90a25944867f3d
H4W9 <hoferwade@gmail.com>
2026-08-13 17:19:29 -0500

    bump version

--- configs.h
+++ configs.h
@@ -17,7 +17,7 @@
 // time; it stays "dev" for local builds.
 #define FW_NAME    "Scrabble"
 #define FW_AUTHOR  "H4W9"
-#define FW_VERSION "0.1.1"
+#define FW_VERSION "0.1.2"
 #ifndef FW_COMMIT
 #define FW_COMMIT  "dev"
 #endif
